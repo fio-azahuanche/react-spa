5a0590b00fdfed6fa91e73d636217688459718d9
Fiorela Azahuanche Falcon <fio-azahuanche@MacBook-Air-de-Fiorela.local>
2023-01-20 15:27:16 -0500

    add hero card

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,17 +1,19 @@
+import { HeroCard } from "./";
 import { getHeroesByPublisher } from "../helpers"
 
 export const HeroList = ({publisher}) => {
     const heroes = getHeroesByPublisher(publisher);
     return (
-        <ul>
+        <div className="row rows-cols-1 row-cols-md-3 g-3">
             {
                 heroes.map( hero => (
-                    <li key={ hero.id }>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard 
+                        key={hero.id}
+                        { ...hero }
+                    />
                 ))
             }
             
-        </ul>
+        </div>
     )
 }
