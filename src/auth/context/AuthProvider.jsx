89c142758f502f03d0152b4f073e178d492a423a
Fiorela Azahuanche Falcon <fio-azahuanche@MacBook-Air-de-Fiorela.local>
2023-01-24 17:03:30 -0500

    keep user active

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,14 +4,22 @@ import { authReducer } from './authReducer';
 
 import { types } from '../types/types';
 
-const initialState = {
-    logged: false,
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return {
+    logged: !!user,
+    user: user
+  }
 }
+
 export const AuthProvider = ({children}) => {
 
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, {}, init );
 
     const login = ( name = '') => {
+      const user = { id: 'ABC', name}
+
       const action = {
         type: types.login,
         payload: {
@@ -19,6 +27,8 @@ export const AuthProvider = ({children}) => {
           name: name
         }
       }
+      localStorage.setItem('user', JSON.stringify(user))
+
       dispatch(action)
     }
 
